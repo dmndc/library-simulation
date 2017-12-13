2c2c570506449b2d6e9a36819c109123fdafe79d
Dino Mandic <dinomandic@hotmail.com>
2017-12-13 15:45:04 -0600

    refactor: structure DetailsView component

--- src/components/DetailsView/DetailsView.js
+++ src/components/DetailsView/DetailsView.js
@@ -5,17 +5,23 @@ import './DetailsView.css';
 class DetailsView extends Component {
   render() {
     return (
-      <nav>
-        <div className="nav-logo">
-         
-        </div>
+      <div>
 
-        <h1>IDEMO MIKI MOJ NAJJACI SI!!!</h1>
+        <h1 className="title">Details<span className="back-button">&#703; Back</span></h1>
 
-        <div className="logout-link">
-          <a href="#">Logout</a>
+        <div className="book-details">
+          <div className="img">
+            <h3>Book img</h3>
+          </div>
+          <div className="info">
+            <h3>Title:</h3>
+            <h3>Author:</h3>
+            <h3>Genre:</h3>
+            <h3>In Stock:</h3>
+            <h3>Description:</h3>
+          </div>
         </div>
-      </nav>
+      </div>
     )
   }
 
