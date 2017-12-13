5e7f695ff1ead46508c220983c1067fc191d7df8
Dino Mandic <dinomandic@hotmail.com>
2017-12-13 15:20:24 -0600

    feat: create DetailsView component

---
+++ src/components/DetailsView/DetailsView.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import './DetailsView.css';
+
+class DetailsView extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-logo">
+         
+        </div>
+
+        <h1>IDEMO MIKI MOJ NAJJACI SI!!!</h1>
+
+        <div className="logout-link">
+          <a href="#">Logout</a>
+        </div>
+      </nav>
+    )
+  }
+
+}
+
+export default DetailsView;
