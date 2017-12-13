dcf11efdf9a4ecf75391a8336a2782ba20a5629a
Dino Mandic <dinomandic@hotmail.com>
2017-12-13 12:38:25 -0600

    fix: add params to endpoint methods

--- server/books_controller.js
+++ server/books_controller.js
@@ -1,16 +1,18 @@
 module.exports = {
   create: (req, res, next) => {
     const dbInstance = req.app.get('db');
+    const { title, author, description, img, in_stock } = req.body;
 
-    dbInstance.create_book()
+    dbInstance.create_book([ title, author, description, img, in_stock ])
       .then( () => res.status(200).send() )
       .catch( () => res.status(500).send() );
   },
 
   getOne: (req, res, next) => {
     const dbInstance = req.app.get('db');
+    const { params } = req;
 
-    dbInstance.read_book()
+    dbInstance.read_book([ params.id ])
       .then( book => res.status(200).send(book) )
       .catch( () => res.status(500).send() );
   },
@@ -25,16 +27,18 @@ module.exports = {
 
   update: (req, res, next) => {
     const dbInstance = req.app.get('db');
+    const { params } = req;
 
-    dbInstance.update_book()
+    dbInstance.update_book([ params.id ])
       .then( () => res.status(200).send() )
       .catch( () => res.status(500).send() );
   },
 
   delete: (req, res, next) => {
     const dbInstance = req.app.get('db');
+    const { params } = req;
 
-    dbInstance.delete_book()
+    dbInstance.delete_book( [params.id] )
       .then( () => res.status(200).send() )
       .catch( () => res.status(500).send() );
   }
