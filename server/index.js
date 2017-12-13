345ea5a3a7a148e415146b938e5754808c6722d6
Dino Mandic <dinomandic@hotmail.com>
2017-12-13 12:22:32 -0600

    feat: add api endpoints to server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const massive = require("massive");
 require('dotenv').config();
 
+// Require books controller
+const booksCtrl = require('./books_controller');
+
 
 const app = express();
 app.use(json());
@@ -13,6 +16,13 @@ app.use(cors());
 massive(process.env.CONNECTION_STRING)
   .then(dbInstance => app.set('db', dbInstance))
   .catch(console.log);
+
+
+app.post('/api/book', booksCtrl.create);
+app.get('/api/books', booksCtrl.getAll);
+app.get('/api/book/:id', booksCtrl.getOne);
+app.put('/api/book/:id', booksCtrl.update);
+app.get('/api/book/:id', booksCtrl.delete);
 
 
 const port = process.env.PORT || 3000;
