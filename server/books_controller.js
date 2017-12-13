27f63888be9c93a836955da64fb180df7820197a
Dino Mandic <dinomandic@hotmail.com>
2017-12-13 13:52:58 -0600

    fix: CRUD methods

--- server/books_controller.js
+++ server/books_controller.js
@@ -25,13 +25,17 @@ module.exports = {
       .catch( () => res.status(500).send() );
   },
 
+  // UPDATE FUNCTION NOT WORKING RIGHT
   update: (req, res, next) => {
     const dbInstance = req.app.get('db');
+    const { title, author, description, img, in_stock } = req.body;
     const { params } = req;
 
-    dbInstance.update_book([ params.id ])
-      .then( () => res.status(200).send() )
+    dbInstance.update_book([ params.id, title, author, description, img, in_stock ])
+      .then( book => res.status(200).send(book) )
       .catch( () => res.status(500).send() );
+
+    console.log(req.body);
   },
 
   delete: (req, res, next) => {
