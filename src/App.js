c17db71014b6e87c3168585fe3514c5e05e8f170
Dino Mandic <dinomandic@hotmail.com>
2017-12-13 15:20:54 -0600

    refactor: clean up the code

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import './App.css';
 // Import components
 import NavBar from './components/NavBar/NavBar';
 
+// Import router
+import router from './router';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
+        <div className="container">
+          { router }
+        </div>
       </div>
     );
   }
