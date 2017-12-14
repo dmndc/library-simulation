c3cc40f403e50cfbe8e519229a632d8ad873802f
Dino Mandic <dinomandic@hotmail.com>
2017-12-14 00:05:31 -0600

    refactor: add new route

--- src/router.js
+++ src/router.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import DetailsView from './components/DetailsView/DetailsView';
+import BrowseView from './components/BrowseView/BrowseView';
 
 export default (
   <Switch>
     <Route component={ DetailsView } path="/book/:id" />
+    <Route component={ BrowseView } path="/browse" />
   </Switch>
 )
